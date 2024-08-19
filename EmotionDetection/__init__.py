c5c4ae645cd021b0004d06b0ac559b358e92b2ef
DhillanC <dhillan.contreras@gmail.com>
2024-08-19 01:59:08 -0400

    Added documentation for testing

--- EmotionDetection/__init__.py
+++ EmotionDetection/__init__.py
@@ -1,1 +1,9 @@
 from . import emotion_detection
+
+"""
+At the terminal type python3.11 to invoke python interpreter.
+At the python prompt type import EmotionDetection
+If the above command runs without errors, it is an indication that the EmotionDetection package is successfully loaded.
+EmotionDetection.emotion_detection.emotion_detector("I hate working long hours")
+You should see an output of the emotion analysis
+"""
